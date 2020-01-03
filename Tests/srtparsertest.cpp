273710e32ca3b1a573b41a3092bb089eb5a46c32
magiruuvelvet <magiruuvelvet@naver.com>
2020-01-03 12:02:49 +0100

    Tests: also validate timestamps

--- Tests/srtparsertest.cpp
+++ Tests/srtparsertest.cpp
@@ -24,7 +24,14 @@ bool parse_basic()
         subs.at(4).subNumber() == 5 &&
         subs.at(253).subNumber() == 254;
 
-    return linesCheck && subNoCheck;
+    // validate timestamps
+    auto timestampCheck =
+        subs.at(0).startTime() == 1626 && subs.at(0).endTime() == 8383 &&
+        subs.at(1).startTime() == 16599 && subs.at(1).endTime() == 18935 &&
+        subs.at(4).startTime() == 24691 && subs.at(4).endTime() == 28403 &&
+        subs.at(253).startTime() == 1262511 && subs.at(253).endTime() == 1264263;
+
+    return linesCheck && subNoCheck && timestampCheck;
 }
 
 } // namespace srtparser_tests
